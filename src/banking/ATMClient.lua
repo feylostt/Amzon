88b236ea597aedd4cf9f235a870f1f99ac99b98e
Lemon <jamiecohen2004@gmail.com>
2022-07-28 16:14:47 -0500

    Update ATMClient.lua

--- src/banking/ATMClient.lua
+++ src/banking/ATMClient.lua
@@ -445,8 +445,8 @@ function deposit()
     print("Please put your diamonds in the compartment above.")
     sleep(5)
 
-    local barrel = peripheral.wrap("minecraft:barrel_0")
-    local vault = peripheral.find("create:item_vault")
+    local barrel = peripheral.find("minecraft:barrel")
+    local vault = peripheral.find("minecraft:vault")
 
     local depositAmount = 0
 
